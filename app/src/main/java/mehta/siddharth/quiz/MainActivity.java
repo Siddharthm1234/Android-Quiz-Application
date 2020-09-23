8cdc440ee7c8323e162e47c5b9919ee0f76fba46
SiddharthMehta <siddharthmehta1234@gmail.com>
2020-09-23 19:02:14 +0530

    Added ViewModel object in MainActivity and added a observer to view changes

--- app/src/main/java/mehta/siddharth/quiz/MainActivity.java
+++ app/src/main/java/mehta/siddharth/quiz/MainActivity.java
@@ -1,15 +1,34 @@
 package mehta.siddharth.quiz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
+
+import mehta.siddharth.quiz.model.Question;
+import mehta.siddharth.quiz.viewmodel.QuestionViewModel;
 
 public class MainActivity extends AppCompatActivity {
+
+    private QuestionViewModel questionViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        questionViewModel = new ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory
+                .getInstance(this.getApplication())).get(QuestionViewModel.class);
+
+        questionViewModel.getAllQuestions().observe(this, new Observer<List<Question>>() {
+            @Override
+            public void onChanged(List<Question> questions) {
+                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
