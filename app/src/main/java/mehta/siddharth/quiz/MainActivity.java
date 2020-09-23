92d56e28ce45afbd91c9c462908a47463eba9874
SiddharthMehta <siddharthmehta1234@gmail.com>
2020-09-24 02:41:36 +0530

    UI changes v4

--- app/src/main/java/mehta/siddharth/quiz/MainActivity.java
+++ app/src/main/java/mehta/siddharth/quiz/MainActivity.java
@@ -4,14 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private Button startButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
